1d8b33af3282079e2d86fd1cef66d4b87788650d
Blastmerder <stepapstear.stepa@yandex.ru>
2025-12-05 14:21:39 +0300

    Damage logic and template for enemy

---
+++ scenes/components/hit_component.gd
@@ -0,0 +1,5 @@
+class_name HitComponent
+extends Area2D
+
+@export var hit_damage: int = 1
+@export var is_owner_player: bool = false
--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -1,10 +1,7 @@
 extends CharacterBody2D
-class_name Player
-
-var player_direction: Vector2 = Vector2.ZERO
-var player_in_dialogue: bool = false
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
+
 func _ready() -> void:
 	hurt_component.on_hurt.connect(on_hurt)
 	damage_component.max_damage_reached.connect(on_max_damage_reached)
@@ -13,4 +10,4 @@ func on_hurt(hit_damage):
 	damage_component.apply_damage(hit_damage)
 
 func on_max_damage_reached():
-	print("Ops! You Die!")
+	queue_free()
