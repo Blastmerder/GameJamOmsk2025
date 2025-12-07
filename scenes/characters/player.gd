d539945bada7161d19060622e7eb5ee6cbaa9a4f
Blastmerder <stepapstear.stepa@yandex.ru>
2025-12-07 15:41:43 +0300

    Clearing and making work together code

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -9,6 +9,7 @@ var player_in_dialogue: bool = false
 @onready var damage_component: DamageComponent = $DamageComponent
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+@export var bullet_scene: PackedScene
 @export var current_tool: DataTypes.Guns = DataTypes.Guns.Pistol
 
 var t = Timer.new()
@@ -31,6 +32,7 @@ func _process(delta: float) -> void:
 	if can_shoot and Input.is_action_pressed("attack"):
 		t.start()
 		can_shoot = false
+		_on_attack(direct())
 		# Here creating bullet
 		# For current logic of gun, use current_tool and compare with DataTypes
 
@@ -42,11 +44,19 @@ func _on_reload_finished():
 func on_hurt(hit_damage):
 	damage_component.apply_damage(hit_damage)
 
+
 func on_max_damage_reached():
 	print("Ops! You Die!")
 
-func _on_attack() -> void:
-	$StateMachine/AttackIdle._on_attack(direct(), global_position)
+
+func _on_attack(direction) -> void:
+	var bullet = bullet_scene.instantiate()
+	bullet.rotation = direction.angle()
+	bullet.position = position + 50 * direction #50 * direct смешение от центра игрока direct нормализован
+	bullet.direction = direction
+	get_tree().current_scene.add_child(bullet)
+	
+	
 func direct():
-	var direction_to_mouse = get_local_mouse_position().normalized() #расчет направления для пули (текущего узла до мыши)
+	var direction_to_mouse = global_position.direction_to(get_global_mouse_position()) #расчет направления для пули (текущего узла до мыши)
 	return direction_to_mouse
