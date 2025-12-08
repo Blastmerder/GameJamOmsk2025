7bd272e6469b3d0c2a1304ae2a0c948c9af5e459
Blastmerder <100530405+Blastmerder@users.noreply.github.com>
2025-12-08 09:58:58 +0300

    Delete scenes directory

--- scenes/components/hit_component.gd
+++
@@ -1,5 +0,0 @@
-class_name HitComponent
-extends Area2D
-
-@export var hit_damage: int = 1
-@export var is_owner_player: bool = false
--- scenes/components/interactable_component.gd
+++
@@ -1,8 +0,0 @@
-class_name InteractableComponent
-extends Area2D
-
-signal interactable_activated
-
-func _on_body_entered(body: Node2D) -> void:
-	print(body)
-	interactable_activated.emit() 
